7a0e8c4474b9daf6d831abeabc1359d331dc5c0a
Carine I. SEMWAGA <semwagacarine@gmail.com>
2019-10-08 09:34:59 +0200

    Create virtual environment

---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/wecode/Documents/moringa/core/django/week1/The-Moringa-Tribune/virtual/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
