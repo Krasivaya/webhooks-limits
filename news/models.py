3c76a3687dcf556e7ca3346d56988e34317502ba
Carine I. SEMWAGA <semwagacarine@gmail.com>
2019-10-08 15:54:50 +0200

    Add article model

--- news/models.py
+++ news/models.py
@@ -17,3 +17,11 @@ class tags(models.Model):
 
     def __str__(self):
         return self.name
+
+#Article model
+class Article(models.Model):
+    title = models.CharField(max_length = 60)
+    post = models.TextField()
+    editor = models.ForeignKey(Editor)
+    tags = models.ManyToManyField(tags)
+    pub_date = models.DateTimeField(auto_now_add=True)
