fe55a963c67e7a11287560aae6d333237aecac07
Carine I. SEMWAGA <semwagacarine@gmail.com>
2019-10-08 13:01:18 +0200

    Define static file

--- tribune/settings.py
+++ tribune/settings.py
@@ -2,20 +2,11 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'a+r)ortg_5&il+&w5y!yso!4zpxt^cl!8_=%jq3(@88+$4*l!='
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
 ALLOWED_HOSTS = []
-
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -108,3 +99,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIR = [
+    os.path.join(BASE_DIR, "static")
+]
