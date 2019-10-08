5ad636562e9c30630dd02e8f15638cce07bf9ba4
Carine I. SEMWAGA <semwagacarine@gmail.com>
2019-10-08 09:51:33 +0200

    Add welcome content into the view function

--- news/views.py
+++ news/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+# News View
+def welcome(request):
+    return HttpResponse('Welcome To The Moringa Tribune')
