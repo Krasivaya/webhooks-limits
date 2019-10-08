f1084d06793e1b8d85c984628948471235f22f23
Carine I. SEMWAGA <semwagacarine@gmail.com>
2019-10-08 18:02:52 +0200

    Remove welcome view function

--- news/views.py
+++ news/views.py
@@ -2,9 +2,6 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse, Http404
 import datetime as dt
 
-# News View
-def welcome(request):
-    return render(request, 'welcome.html')
 
 #News view for particular day
 def news_of_day(request):
