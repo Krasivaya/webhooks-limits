01cacf3eb6c0a2f08be31d49e0b5a5ef4f809fe2
Carine I. SEMWAGA <semwagacarine@gmail.com>
2019-10-08 17:43:15 +0200

    Add editor test

--- news/tests.py
+++ news/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from .models import Editor, Article, tags
 
-# Create your tests here.
+# Editor test
+class EditorTestClass(TestCase):
+
+    # Set up method
+    def setUp(self):
+        self.Carine= Editor(first_name = 'Carine', last_name ='I. SEMWAGA', email ='semwagacarine@gmail.com')
