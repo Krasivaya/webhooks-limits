8fc23da0537a8e1fed14c094d80f4dd4ef5f9205
Carine I. SEMWAGA <semwagacarine@gmail.com>
2019-10-08 09:59:27 +0200

    Add news view function for a particular day

--- news/views.py
+++ news/views.py
@@ -1,5 +1,18 @@
 from django.http import HttpResponse
+import datetime as dt
 
 # News View
 def welcome(request):
     return HttpResponse('Welcome To The Moringa Tribune')
+
+#News view for particular day
+def news_of_day(request):
+    date = dt.date.today()
+    html = f'''
+        <html>
+            <body>
+                <h1> {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+            '''
+    return HttpResponse(html)
