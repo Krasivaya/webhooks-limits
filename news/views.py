d2bac2273b37226b197ed7c631b63e37f5fdbaba
Carine I. SEMWAGA <semwagacarine@gmail.com>
2019-10-08 10:29:39 +0200

    Add 404 page for invalid number for date

--- news/views.py
+++ news/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 import datetime as dt
 
 # News View
@@ -33,8 +33,12 @@ def convert_dates(dates):
 
 #Passt days News View
 def past_days_news(request, past_date):
+    try:
         #convert data from url
         date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+    except ValueError:
+        #raise 404 error when valueError is thrown
+        raise Http404()
     day = convert_dates(date)
     hrml = f'''
         <html>
