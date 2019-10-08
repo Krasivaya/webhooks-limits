4d1610957e6dc80f16bbfe74fd10f06867885b14
Carine I. SEMWAGA <semwagacarine@gmail.com>
2019-10-08 18:29:51 +0200

    Add test for article model

--- news/tests.py
+++ news/tests.py
@@ -17,3 +17,26 @@ class EditorTestClass(TestCase):
         self.Carine.save_editor()
         editors = Editor.objects.all()
         self.assertTrue(len(editors) > 0)
+
+
+# Articles test
+class ArticleTestClass(TestCase):
+
+    def setUp(self):
+        # Creating a new editor and saving it
+        self.Carine= Editor(first_name = 'Carine', last_name ='I. SEMWAGA', email ='semwagacarine@gmail.com')
+        self.Carine.save_editor()
+
+        # Creating a new tag and saving it
+        self.new_tag = tags(name = 'testing')
+        self.new_tag.save()
+
+        self.new_article= Article(title = 'Test Article',post = 'This is a random test Post',editor = self.Carine)
+        self.new_article.save()
+
+        self.new_article.tags.add(self.new_tag)
+
+    def tearDown(self):
+        Editor.objects.all().delete()
+        tags.objects.all().delete()
+        Article.objects.all().delete()
