931b08e08cb4adf1e94c422a49ba6959c5151a87
Krasivaya <semwagacarine@gmail.com>
2019-10-10 13:08:51 +0200

    Add view funciton for a single article

--- news/views.py
+++ news/views.py
@@ -39,3 +39,12 @@ def search_results(request):
     else:
         message = "You haven't searched for any term"
         return render(request, 'all-news/search.html',{"message":message})
+
+#A single article view
+
+def article(request,article_id):
+    try:
+        article = Article.objects.get(id = article_id)
+    except DoesNotExist:
+        raise Http404()
+    return render(request,"all-news/article.html", {"article":article})
