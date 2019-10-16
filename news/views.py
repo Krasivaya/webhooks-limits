1a955922d93acdcfc36656edde1d91a8d8094d03
Carine I. SEMWAGA <semwagacarine@gmail.com>
2019-10-16 13:38:37 +0200

    Add form view function

--- news/views.py
+++ news/views.py
@@ -2,13 +2,20 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse, Http404
 import datetime as dt
 from .models import Article
-
+from .forms import NewsLetterForm
 
 #News view for particular day
+#Form view
 def news_today(request):
     date = dt.date.today()
     news = Article.todays_news()
-    return render(request,'all-news/today-news.html', {"date":date,"news":news})
+    if request.method == 'POST':
+        form = NewsLetterForm(request.POST)
+        if form.is_valid():
+            print('valid')
+    else:
+        form = NewsLetterForm()
+    return render(request, 'all-news/today-news.html', {"date": date,"news":news,"letterForm":form})
 
 #Passt days News View
 def past_days_news(request, past_date):
