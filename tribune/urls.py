0938b0dd4abc278982c1e2b2d636966fbae6525a
Carine I. SEMWAGA <semwagacarine@gmail.com>
2019-10-17 12:12:51 +0200

    Import views file

--- tribune/urls.py
+++ tribune/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url, include
 from django.contrib import admin
-from . import views
+from django.contrib.auth import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
