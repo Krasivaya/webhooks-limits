90586656abc73d2bad5ae531e1f9ccd22da8e559
Carine I. SEMWAGA <semwagacarine@gmail.com>
2019-10-17 12:44:15 +0200

    Update articles view function

--- news/views.py
+++ news/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse, Http404,HttpResponseRedirect
 import datetime as dt
 from .models import Article, NewsLetterRecipients
-from .forms import NewsLetterForm
+from .forms import NewArticleForm, NewsLetterForm
 from .email import send_welcome_email
 from django.contrib.auth.decorators import login_required
 
@@ -56,9 +56,16 @@ def search_results(request):
 
 #A single article view
 @login_required(login_url='/accounts/login/')
-def article(request,article_id):
-    try:
-        article = Article.objects.get(id = article_id)
-    except DoesNotExist:
-        raise Http404()
-    return render(request,"all-news/article.html", {"article":article})
+def new_article(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = NewArticleForm(request.POST, request.FILES)
+        if form.is_valid():
+            article = form.save(commit=False)
+            article.editor = current_user
+            article.save()
+        return redirect('NewsToday')
+
+    else:
+        form = NewArticleForm()
+    return render(request, 'new_article.html', {"form": form})
