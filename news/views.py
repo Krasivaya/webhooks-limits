06795725ee8f650f96aa3fd61d46b4c47b25ba4c
Carine I. SEMWAGA <semwagacarine@gmail.com>
2019-10-23 11:11:20 +0200

    Update news_toaday view fcuntion

--- news/views.py
+++ news/views.py
@@ -8,12 +8,22 @@ from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
 
 #News view for particular day
-#Form view
 def news_today(request):
     date = dt.date.today()
     news = Article.todays_news()
     form = NewsLetterForm()
     return render(request, 'all-news/today-news.html', {"date": date,"news":news,"letterForm":form})
+
+#Form view
+def newsletter(request):
+    name = request.POST.get('your_name')
+    email = request.POST.get('email')
+
+    recipient = NewsLetterRecipients(name=name, email=email)
+    recipient.save()
+    send_welcome_email(name, email)
+    data = {'success': 'You have been successfully added to mailing list'}
+    return JsonResponse(data)
 
 #Passt days News View
 def past_days_news(request, past_date):
