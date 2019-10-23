39447bbce91eb69c1502e70de44b3cb07bbb9e0b
Carine I. SEMWAGA <semwagacarine@gmail.com>
2019-10-23 12:32:13 +0200

    Add Description view class

--- news/views.py
+++ news/views.py
@@ -49,6 +49,20 @@ class MerchList(APIView):
             return Response(serializers.data, status=status.HTTP_201_CREATED)
         return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
 
+#Description view class
+class MerchDescription(APIView):
+    permission_classes = (IsAdminOrReadOnly,)
+    def get_merch(self, pk):
+        try:
+            return MoringaMerch.objects.get(pk=pk)
+        except MoringaMerch.DoesNotExist:
+            return Http404
+
+    def get(self, request, pk, format=None):
+        merch = self.get_merch(pk)
+        serializers = MerchSerializer(merch)
+        return Response(serializers.data)
+
 #Passt days News View
 def past_days_news(request, past_date):
     try:
